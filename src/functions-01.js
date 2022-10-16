b6760728e5b9f5ab85fdd6b6f1af71a8c6011bb1
David DiBenedetto <david@njweb.solutions>
2022-10-16 17:40:48 -0400

    Pass all tests with 100% coverage
    Write function bodies and return statements to pass all tests

--- src/functions-01.js
+++ src/functions-01.js
@@ -16,7 +16,8 @@
  * example: 'Buzz Lightyear' returns 'Lightyear, Buzz'
  */
 function nameShuffle(str) {
-  // write your code here & return value
+  const nameAr = str.split(' ');
+  return `${nameAr[1]}, ${nameAr[0]}`;
 }
 
 /**
@@ -34,7 +35,7 @@ function nameShuffle(str) {
  * example: '&', '&' returns true
  */
 function isStrangePair(str1, str2) {
-  // write your code here & return value
+  return str1[0] === str2[str2.length - 1] || str2[0] === str1[str1.length - 1];
 }
 
 /**
@@ -46,7 +47,7 @@ function isStrangePair(str1, str2) {
  * example: '98.6%' returns 0.986
  */
 function convertToDecimal(percent) {
-  // write your code here & return value
+  return parseFloat(percent) / 100;
 }
 
 /**
@@ -59,7 +60,15 @@ function convertToDecimal(percent) {
  * example: [1, 2, 3, 4], [4, 3, 2, 1, 5] returns false
  */
 function checkSameSum(a1, a2) {
-  // write your code here & return value
+  let sum1 = 0;
+  let sum2 = 0;
+  for (let i = 0; i < a1.length; i += 1) {
+    sum1 += a1[i];
+  }
+  for (let j = 0; j < a2.length; j += 1) {
+    sum2 += a2[j];
+  }
+  return sum1 === sum2;
 }
 
 /**
@@ -70,7 +79,9 @@ function checkSameSum(a1, a2) {
  * must use a closure to save the username
  */
 function saveLogin(name) {
-  // write your code here
+  return function myFunc() {
+    return name;
+  };
 }
 
 module.exports = {
@@ -78,5 +89,5 @@ module.exports = {
   isStrangePair,
   convertToDecimal,
   checkSameSum,
-  saveLogin
+  saveLogin,
 };
--- src/functions-02.js
+++ src/functions-02.js
@@ -69,7 +69,7 @@ const store = {
    * @returns {string} - the name of the store
    */
   getName() {
-    // write your code here & return value
+    return this.name;
   },
   /**
    * Returns the inventory of the store
@@ -77,7 +77,7 @@ const store = {
    * @returns {array} - the inventory of the store
    */
   getInventory() {
-    // write your code here & return value
+    return inventory;
   },
   /**
    * Returns an arrays of most expensive items in inventory
@@ -86,7 +86,7 @@ const store = {
    * @return {array} items - the array of items that are filtered
    */
   getExpensiveItems(minPrice) {
-    // write your code here & return value
+    return inventory.filter((item) => item.price > minPrice);
   },
   /**
    * Returns an array of item names in store
@@ -94,7 +94,7 @@ const store = {
    * @return {array} items - the array of items that are filtered
    */
   getStoreItems() {
-    // write your code here & return value
+    return inventory.map((item) => item.name);
   },
   /**
    * Returns true if the item is in the store
@@ -104,7 +104,7 @@ const store = {
    * false otherwise
    */
   isItemInStore(itemName) {
-    // write your code here & return value
+    return this.getStoreItems().includes(itemName);
   },
   /**
    * Returns the price of the item
@@ -115,7 +115,12 @@ const store = {
    * must use isItemInStore() method in this object
    */
   getItemPrice(itemName) {
-    // write your code here & return value
+    for (let i = 0; i < inventory.length; i += 1) {
+      if (inventory[i].name === itemName) {
+        return inventory[i].price;
+      }
+    }
+    return -1;
   },
 
   /**
@@ -127,7 +132,12 @@ const store = {
    * must use isItemInStore() method in this object
    */
   getItemQuantity(itemName) {
-    // write your code here & return value
+    for (let i = 0; i < inventory.length; i += 1) {
+      if (inventory[i].name === itemName) {
+        return inventory[i].quantity;
+      }
+    }
+    return -1;
   },
 
   /**
@@ -142,29 +152,54 @@ const store = {
    * must use isItemInStore() method in this object
    */
   addItemQuantity(itemName, price, quantity) {
-    // write your code here & return value
+    if (!this.isItemInStore(itemName)) {
+      inventory[inventory.length] = { name: itemName, price, quantity };
+      return quantity;
+    }
+    let myQuantity;
+    for (let i = 0; i < inventory.length; i += 1) {
+      if (inventory[i].name === itemName) {
+        inventory[i].price = price;
+        inventory[i].quantity += quantity;
+        myQuantity = inventory[i].quantity;
+        break;
+      }
+    }
+    return myQuantity;
   },
   /**
-   * Removes a certain quantity of an item from the store
-   * @method removeItemQuantity
-   * @param {string} itemName - name of the item to remove from store
-   * @param {number} quantity - the quantity of the items to remove
-   * @return {number} newQuantity - the quantity of the item
-   * after processing, or -1 if the item is not in the store
-   * or -1 if the quantity to remove is greater than the quantity of the item
-   * must use isItemInStore() method in this object
-   */
+     * Removes a certain quantity of an item from the store
+     * @method removeItemQuantity
+     * @param {string} itemName - name of the item to remove from store
+     * @param {number} quantity - the quantity of the items to remove
+     * @return {number} newQuantity - the quantity of the item
+     * after processing, or -1 if the item is not in the store
+     * or -1 if the quantity to remove is greater than the quantity of the item
+     * must use isItemInStore() method in this object
+     */
   removeItemQuantity(itemName, quantity) {
-    // write your code here & return value
+    if (!this.isItemInStore(itemName)) {
+      return -1;
+    }
+    const myItem = inventory.find((item) => item.name === itemName);
+    if (quantity > myItem.quantity) {
+      return -1;
+    }
+    myItem.quantity -= quantity;
+    return myItem.quantity;
   },
   /**
-   * Returns the total of all the items in the store
-   * @method getTotalValue
-   * @return {number} totalPrice - the total price of the items in the store
-   * must use the reduce() array method
-   */
+     * Returns the total of all the items in the store
+     * @method getTotalValue
+     * @return {number} totalPrice - the total price of the items in the store
+     * must use the reduce() array method
+     */
   getTotalValue() {
-    // write your code here & return value
+    let sum = 0;
+    for (let i = 0; i < inventory.length; i += 1) {
+      sum += inventory[i].price * inventory[i].quantity;
+    }
+    return sum;
   },
 };
 
